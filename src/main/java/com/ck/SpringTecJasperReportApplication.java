cac40450f554f79021a146211788376011e78fed
Chuan <luuhuuchuan@gmail.com>
2018-12-26 17:15:53 +0700

    first

---
+++ src/main/java/com/ck/SpringTecJasperReportApplication.java
@@ -0,0 +1,14 @@
+package com.ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTecJasperReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTecJasperReportApplication.class, args);
+	}
+
+}
+
